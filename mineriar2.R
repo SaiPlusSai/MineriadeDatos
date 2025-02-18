d8d89cc8196816750d444a2cf2ce7b1423f23aa6
SaiPlusSai <refgu774@gmail.com>
2025-02-18 08:46:42 -0400

    parte2

--- mineriar2.R
+++ mineriar2.R
@@ -4,4 +4,17 @@ library(tm)#Mineria de texto
 library(udpipe)#Normalizar o etiquetado
 library(hunspell)#Ortografia
 library(syuzhet)#Analisis se sentimiento
-load()
+options(StringAsFactors=FALSE)
+Sys.setlocale(category = "LC_ALL", locale = "es_ES.UTF-8")
+load("larazon.RData")
+###############################
+#ortografia
+texto<-"ola mano que fue abla  mi eztimadho"
+list_dictionaries()
+aux<-hunspell(texto,dict="es_ES")
+hunspell_suggest(aux[[1]],dict="es_ES")
+bd$titular
+hunspell(bd$titular[1:5],dict="es_ES")
+#Asume que cada titular ya es un documento aparte
+corpus_larazon<-VCorpus(VectorSource(bd$titular))
+corpus_larazon[[20]]$content
