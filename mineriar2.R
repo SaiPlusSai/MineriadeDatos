7ea522272b4c98c47bd15c335ac3b3efae46af1f
SaiPlusSai <refgu774@gmail.com>
2025-02-18 08:28:08 -0400

    clase

---
+++ mineriar2.R
@@ -0,0 +1,7 @@
+rm(list = ls())
+library(dplyr)
+library(tm)#Mineria de texto
+library(udpipe)#Normalizar o etiquetado
+library(hunspell)#Ortografia
+library(syuzhet)#Analisis se sentimiento
+load()
